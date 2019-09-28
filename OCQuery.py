17754530fea8cf541ef61def0f4adf6e18209f89
Skickar <40251293+skickar@users.noreply.github.com>
2019-09-28 09:42:55 -0700

    Formatted address, omitted redundant address
    great job

--- OCQuery.py
+++ OCQuery.py
@@ -2,6 +2,7 @@
 ## Requires no API key but may be rate limited after several searches
 ## If you have a VPN, turn it on and change regions before using to avoid your primary IP being limited
 
+
 import requests, json
 
 companySearch = input("Name of company to search?\n>")
@@ -13,7 +14,7 @@ for i in range(len(data['results']['companies'])):
     print("Current status: ",data['results']['companies'][i]['company']['current_status'])
     print("Jurisdiction: ",data['results']['companies'][i]['company']['jurisdiction_code'])
     print("Active from: ",data['results']['companies'][i]['company']['incorporation_date'], " to ",data['results']['companies'][i]['company']['dissolution_date'])
-    print("Address: ",data['results']['companies'][i]['company']['registered_address'])
+    print("Address: ",data['results']['companies'][i]['company']['registered_address']['street_address'], ",",data['results']['companies'][i]['company']['registered_address']['country'])
     print("Data Source: ", data['results']['companies'][i]['company']['source']['publisher'], "at link ",data['results']['companies'][i]['company']['source']['url'])
     print("Opencorporates URL: ", data['results']['companies'][i]['company']['opencorporates_url'])
     filings = (requests.get("https://api.opencorporates.com/v0.4/companies/{}/{}/filings".format(data['results']['companies'][i]['company']['jurisdiction_code'],data['results']['companies'][i]['company']['company_number']))).json();
